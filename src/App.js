a72510ee3de6158d9ef73cb7ca0d29b97e9dc237
unknown <ignatius.andika@gmail.com>
2022-09-10 13:22:31 +0700

    update cart caribarang

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { CartProvider } from "react-use-cart";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../src/App.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Home from './pages/Home'
+import Cart from "./pages/Cart";
 
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    return (
+        <>
+            <BrowserRouter>
+            <Navbar bg="light" expand="lg" className='navbar'>
+              <Container fluid>
+                <Navbar.Brand href="#"><h2>CariBarang.ID</h2></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                    className="me-auto my-2 my-lg-0"
+                    navbarScroll
+                  >
+                    <NavLink to="/" style={{color:'black', textDecoration:'none'}}>
+                            <h4>Product</h4>
+                    </NavLink>
+                  </Nav>
+                    <Button variant="outline-success" className="d-flex">
+                    <NavLink to="/cart" style={{color:'black', textDecoration:'none'}}>
+                            <h4 className="m-0">Cart</h4>
+                        </NavLink>
+                        </Button>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+                <CartProvider>
+                  <Routes>
+                      <Route exact path="/" element={<Home />} />
+                      <Route exact path="/cart" element={<Cart />} />
+                  </Routes>
+                </CartProvider>
+            </BrowserRouter>
+        </>
+    );
 }
-
+  
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,71 @@
+import imgRC from '../src/assets/RC-skylinegtr.jpg'
+import imgGA from '../src/assets/accoustic-guitar-mexico.jpg'
+import imgSB from '../src/assets/tonyhawk-skateboard.jpg'
+import imgAH from '../src/assets/arai_helm_arai.jpg'
+import imgHB from '../src/assets/hiking-bag.jpg'
+import imgRS from '../src/assets/roller-skate.jpg'
+import imgSurf from '../src/assets/surfboard.jpg'
+import imgKG from '../src/assets/keyboard-gaming.jpg'
+
+const Data = {
+    productData:[
+        {
+            id: 1,
+            img: imgRC,
+            title: 'RC Toys Car',
+            desc: 'Toys car electric remote control',
+            price: '450000'
+        },
+        {
+            id: 2,
+            img: imgGA,
+            title: 'Guitar Accoustic',
+            desc: 'Guitar accoustic from Mexico from phinus wood',
+            price: '6550000'
+        },
+        {
+            id: 3,
+            img: imgSB,
+            title: 'Skateboard Tony Hawk',
+            desc: 'Skateboard limited edition Tony Hawk Anniversary',
+            price: '5400000'
+        },
+        {
+            id: 4,
+            img: imgAH,
+            title: 'Helm Arai RX7X',
+            desc: 'Racing bike helmed by Arai',
+            price: '9800000'
+        },
+        {
+            id: 5,
+            img: imgHB,
+            title: 'Hiking Bag',
+            desc: 'Bag for hiking mountain',
+            price: '850000'
+        },
+        {
+            id: 6,
+            img: imgRS,
+            title: 'Roller Skate',
+            desc: 'Roller skate rainbow for women',
+            price: '520000'
+        },
+        {
+            id: 7,
+            img: imgSurf,
+            title: 'Surf Board',
+            desc: 'Surf board fo professinoal surfer',
+            price: '9200000'
+        },
+        {
+            id: 8,
+            img: imgKG,
+            title: 'Keyboard Gaming',
+            desc: 'Keyboard mechanical for professional gaming',
+            price: '420000'
+        }
+    ]
+}
+
+export default Data;
